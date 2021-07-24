2ad7e6cf8c246d23368469e0b1bde85ac7cfded4
Dreamlanding <112757530+Dreamlanding@users.noreply.github.com>
2021-07-24 20:45:13 +0000

    Dockerfile added

--- keras/keras_xor.py
+++ keras/keras_xor.py
@@ -6,4 +6,6 @@ Created on Sun May 28 14:01:01 2017
 
 https://gist.github.com/stewartpark/187895beb89f0a1b3a54
 """
-fro
+from keras import backend as K
+from keras.models import Sequential
+from keras.layers i
