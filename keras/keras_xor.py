0ebee5338923afda105affec985c525504939046
Dreamlanding <112757530+Dreamlanding@users.noreply.github.com>
2021-07-25 20:30:27 +0000

    Update Dao.sol

--- keras/keras_xor.py
+++ keras/keras_xor.py
@@ -17,4 +17,6 @@ num_classes = 1
 epochs = 1000
 
 x_train = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
-y_train = 
+y_train = np.array([[0], [1], [1], [0]])
+x_test = np.array([[0, 0], [1, 0]])
+y_test = np.array([[0], [1]
