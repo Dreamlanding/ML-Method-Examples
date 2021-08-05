104993311eb5a2890221dc8d86dc719e84456d92
Dreamlanding <112757530+Dreamlanding@users.noreply.github.com>
2021-08-05 23:10:41 +0000

    Pontoon: Update Slovak (sk) localization of Common Voice

--- keras/keras_xor.py
+++ keras/keras_xor.py
@@ -48,4 +48,7 @@ model.compile(loss='binary_crossentropy',
 history = model.fit(x_train, y_train,
                     batch_size=batch_size,
                     epochs=epochs,
-                   
+                    verbose=1,
+                    validation_data=(x_test, y_test))
+
+# 利用测试样本评价模型，可以计算loss值以及测试集中
