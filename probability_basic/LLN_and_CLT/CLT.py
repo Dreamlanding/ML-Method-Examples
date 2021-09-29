707c46c35ec48ed42a06d1430f411e0ab5d02a67
Dreamlanding <112757530+Dreamlanding@users.noreply.github.com>
2021-09-29 15:00:04 +0000

    Update variables.scss

--- probability_basic/LLN_and_CLT/CLT.py
+++ probability_basic/LLN_and_CLT/CLT.py
@@ -77,4 +77,5 @@ single_sample_dist = stats.uniform(loc=uniform_para[0], scale=uniform_para[1])  
 # 下面是利用matplotlib画图
 plt.figure(1)
 plt = plot(n=size[0], dist=single_sample_dist, subplot=321, plt_handle=plt, dist_type=dist_type)
-plt = plot(n=size[1], dist=sin
+plt = plot(n=size[1], dist=single_sample_dist, subplot=322, plt_handle=plt, dist_type=dist_type)
+plt = plot(n=size[2]
