f530f144e4673286df2d5762d3927f7919604444
Dreamlanding <112757530+Dreamlanding@users.noreply.github.com>
2021-10-03 10:36:16 +0000

    Update node.js.yml

--- probability_basic/LLN_and_CLT/CLT.py
+++ probability_basic/LLN_and_CLT/CLT.py
@@ -79,4 +79,5 @@ plt.figure(1)
 plt = plot(n=size[0], dist=single_sample_dist, subplot=321, plt_handle=plt, dist_type=dist_type)
 plt = plot(n=size[1], dist=single_sample_dist, subplot=322, plt_handle=plt, dist_type=dist_type)
 plt = plot(n=size[2], dist=single_sample_dist, subplot=323, plt_handle=plt, dist_type=dist_type)
-plt = plot(n=size[3], dist=single_sample_dist, subplot=324, plt_handle=plt, dist_type=di
+plt = plot(n=size[3], dist=single_sample_dist, subplot=324, plt_handle=plt, dist_type=dist_type)
+plt = plot(n=size[4], dist=single_sample_dist, subplot=325, plt_handle=plt, dist_ty
