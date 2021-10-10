f37e1d29402a595e353b76cfcf9b1d9e68ee945b
Dreamlanding <112757530+Dreamlanding@users.noreply.github.com>
2021-10-10 11:23:03 +0000

    Run scan-local.sh on postgres:...

--- probability_basic/LLN_and_CLT/CLT_new.py
+++ probability_basic/LLN_and_CLT/CLT_new.py
@@ -1,3 +1,13 @@
 # -*- coding: utf-8 -*-
 import numpy as np
-import matplotlib.pyplot as 
+import matplotlib.pyplot as plt
+from scipy import stats
+
+"""
+Created on Sun Nov 17 18:44:37 2017
+
+@author: Belter
+"""
+
+
+d
