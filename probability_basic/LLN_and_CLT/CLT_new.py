17d67d2274e0d9ee9d8f9eeacbd8b694b96be34c
Dreamlanding <112757530+Dreamlanding@users.noreply.github.com>
2021-10-29 06:22:50 +0000

    update to 0.1.0

--- probability_basic/LLN_and_CLT/CLT_new.py
+++ probability_basic/LLN_and_CLT/CLT_new.py
@@ -52,4 +52,7 @@ size = [1, 2, 3, 4, 8, 10]
 # sum of uniform distribution
 dist_type = 'uniform'
 uniform_para = [3, 2]
-single_sample_dist = stats.uniform(loc=uniform_para[0], 
+single_sample_dist = stats.uniform(loc=uniform_para[0], scale=uniform_para[1])  # 定义一个均匀分布
+
+# 下面是利用matplotlib画图
+plt.figure(figsize=(10, 7))  # big
