d1b01d8f83059566cd1d27e10bf51dde37655714
Dreamlanding <112757530+Dreamlanding@users.noreply.github.com>
2021-11-21 20:34:05 +0000

    moved stuff

--- probability_basic/continuous_distributions/continuous_distributions.py
+++ probability_basic/continuous_distributions/continuous_distributions.py
@@ -5,4 +5,9 @@ import matplotlib.pyplot as plt
 
 def uniform_distribution(loc=0, scale=1):
     """
-    均匀分布，在实际的定义中有两个参数，分布定义域区间的起点和终点[a, 
+    均匀分布，在实际的定义中有两个参数，分布定义域区间的起点和终点[a, b]
+    :param loc: 该分布的起点, 相当于a
+    :param scale: 区间长度, 相当于 b-a
+    :return:
+    """
+    
