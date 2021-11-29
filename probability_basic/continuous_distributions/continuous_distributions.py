1a9dbb654e15dd596f1e32657e1c1ae8511fc5c1
Dreamlanding <112757530+Dreamlanding@users.noreply.github.com>
2021-11-29 17:33:13 +0000

    added directories

--- probability_basic/continuous_distributions/continuous_distributions.py
+++ probability_basic/continuous_distributions/continuous_distributions.py
@@ -45,4 +45,8 @@ def exponential_dis(loc=0, scale=1.0):
     指数分布，exponential continuous random variable
     按照定义，指数分布只有一个参数lambda，这里的scale = 1/lambda
     :param loc: 定义域的左端点，相当于将整体分布沿x轴平移loc
-    :param scale: lambda的倒数，loc + scale表
+    :param scale: lambda的倒数，loc + scale表示该分布的均值，scale^2表示该分布的方差
+    :return:
+    """
+    exp_dis = stats.expon(loc=loc, scale=scale)
+    x 
