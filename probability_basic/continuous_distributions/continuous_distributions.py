b2f4adb1a4085fdab11bdccf40a9935d48f92f17
Dreamlanding <112757530+Dreamlanding@users.noreply.github.com>
2022-01-01 18:59:04 +0000

    regexp

--- probability_basic/continuous_distributions/continuous_distributions.py
+++ probability_basic/continuous_distributions/continuous_distributions.py
@@ -155,4 +155,5 @@ def diff_normal_dis():
     x3 = np.linspace(norm_dis_3.ppf(1e-6), norm_dis_3.ppf(0.999999), 1000)
     fig, ax = plt.subplots(1, 1)
     ax.plot(x0, norm_dis_0.pdf(x0), 'r-', lw=2, label=r'miu=0, sigma=1')
-    ax.plot(x1, no
+    ax.plot(x1, norm_dis_1.pdf(x1), 'b-', lw=2, label=r'miu=0, sigma=0.5')
+    ax.plot(x2, norm_dis_2.p
