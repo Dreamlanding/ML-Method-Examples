2b60191fe06070ae707c822248e8b0d2de849416
Dreamlanding <112757530+Dreamlanding@users.noreply.github.com>
2022-01-07 13:05:20 +0000

    More styling

--- probability_basic/discrete_distributions/discrete_distributions.py
+++ probability_basic/discrete_distributions/discrete_distributions.py
@@ -1,3 +1,11 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sun Jul 16 18:4
+Created on Sun Jul 16 18:47:10 2017
+
+@author: xin
+"""
+
+# an example
+import numpy as np
+from scipy import stats
+import matpl
