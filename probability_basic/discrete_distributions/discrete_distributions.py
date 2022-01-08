a035e510083f871fe3bf841547c3c6616f7f6578
Dreamlanding <112757530+Dreamlanding@users.noreply.github.com>
2022-01-08 12:45:17 +0000

    add MIT license file

--- probability_basic/discrete_distributions/discrete_distributions.py
+++ probability_basic/discrete_distributions/discrete_distributions.py
@@ -8,4 +8,9 @@ Created on Sun Jul 16 18:47:10 2017
 # an example
 import numpy as np
 from scipy import stats
-import matpl
+import matplotlib.pyplot as plt
+
+
+def example1():
+    # 分布的参数初始化
+    myDF = stats.norm(5, 3)
