3a069a307c0283365bd55ace62f1039a8fc7666f
Dreamlanding <112757530+Dreamlanding@users.noreply.github.com>
2022-01-08 16:48:30 +0000

    Fixes links

--- probability_basic/discrete_distributions/discrete_distributions.py
+++ probability_basic/discrete_distributions/discrete_distributions.py
@@ -13,4 +13,7 @@ import matplotlib.pyplot as plt
 
 def example1():
     # 分布的参数初始化
-    myDF = stats.norm(5, 3)
+    myDF = stats.norm(5, 3)  # Create the frozen distribution
+    # 取101个等间距的x
+    X = np.linspace(-5, 15, 101)
+    # cdf, 累计分
