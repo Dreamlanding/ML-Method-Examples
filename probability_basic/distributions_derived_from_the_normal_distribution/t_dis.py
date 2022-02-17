c4e099b8a277c9b306abb05558ff162b47000978
Dreamlanding <112757530+Dreamlanding@users.noreply.github.com>
2022-02-17 16:56:51 +0000

    align center

--- probability_basic/distributions_derived_from_the_normal_distribution/t_dis.py
+++ probability_basic/distributions_derived_from_the_normal_distribution/t_dis.py
@@ -6,4 +6,8 @@ def calculate_t_score():
     """
     计算上alpha分位数值，相当于已知某个点的上分位数(1-cdf)，求对于的t score
     :return:
-    "
+    """
+    n = 20
+    df = n - 1  # 自由度
+    alpha = 0.05
+    t_score = stats.t(df).isf(alpha
