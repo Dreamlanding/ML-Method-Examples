b78c48b837719c1125fb661f35620278276c4a8c
Dreamlanding <112757530+Dreamlanding@users.noreply.github.com>
2023-03-19 13:30:20 +0000

    New CV

---
+++ scikit_learn/ch2_end_to_end_ML_project/get_the_data.py
@@ -0,0 +1,39 @@
+
+import os
+import tarfile
+from six.moves import urllib
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import hashlib
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import StratifiedShuffleSplit
+from pandas.plotting import scatter_matrix
+from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import OneHotEncoder
+
+
+DOWNLOAD_ROOT = 'https://raw.githubusercontent.com/ageron/handson-ml/master/'
+HOUSING_PATH = 'datasets/housing'
+HOUSING_URL = DOWNLOAD_ROOT + HOUSING_PATH + '/housing.tgz'
+
+
+def fetch_housing_data(housing_url=HOUSING_URL, housing_path=HOUSING_PATH):
+    """
+    download raw data and unpack
+    :param housing_url:
+    :param housing_path:
+    :return:
+    """
+    if not os.path.isdir(housing_path):
+        os.makedirs(housing_path)
+    tgz_path = os.path.join(housing_path, 'housing.tgz')
+    urllib.request.urlretrieve(housing_url, tgz_path)
+    housing_tgz = tarfile.open(tgz_path)
+    housing_tgz.extractall(path=housing_path)
+    housing_tgz.close()
+
+
+def load_housing_data(housing_path=HOUSING_PATH):
+    csv_path = os.path.join(housing_path, 'housing.csv')
+    return pd.read_csv(csv_path)
