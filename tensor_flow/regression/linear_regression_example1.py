a205a74dc6d2200be2e949d7d23b6197f345047b
Dreamlanding <112757530+Dreamlanding@users.noreply.github.com>
2023-05-14 15:09:31 +0000

    all working

--- tensor_flow/regression/linear_regression_example1.py
+++ tensor_flow/regression/linear_regression_example1.py
@@ -8,4 +8,10 @@ import tensorflow as tf
 
 # define input data
 X_data = np.arange(100, step=.1)
-y_data = X_data + 20 * np.sin(X
+y_data = X_data + 20 * np.sin(X_data/10)
+
+# define data size and batch size
+n_samples = 1000
+batch_size = 100
+
+# tensorflow is fini
