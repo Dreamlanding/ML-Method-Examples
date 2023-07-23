14cd08fc2cae9361b3bedf6a0842508842f378eb
Dreamlanding <112757530+Dreamlanding@users.noreply.github.com>
2023-07-23 11:09:44 +0000

    new version

--- tensor_flow/save_and_restore/save_reuse_parameters.py
+++ tensor_flow/save_and_restore/save_reuse_parameters.py
@@ -1,1 +1,4 @@
-# this example comes from book, "Machine Learning with Sciki
+# this example comes from book, "Machine Learning with Scikit-Learn and TensorFlow"
+
+import numpy as np
+from sklearn.datasets import fetch_californi
