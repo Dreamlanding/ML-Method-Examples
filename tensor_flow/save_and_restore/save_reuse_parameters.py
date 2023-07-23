1094a1b7256a55072717ffed0db015c3a619f027
Dreamlanding <112757530+Dreamlanding@users.noreply.github.com>
2023-07-23 22:53:47 +0000

    Update linphone submodule.

--- tensor_flow/save_and_restore/save_reuse_parameters.py
+++ tensor_flow/save_and_restore/save_reuse_parameters.py
@@ -1,4 +1,6 @@
 # this example comes from book, "Machine Learning with Scikit-Learn and TensorFlow"
 
 import numpy as np
-from sklearn.datasets import fetch_californi
+from sklearn.datasets import fetch_california_housing
+import tensorflow as tf
+from sklearn.preprocessing import StandardScaler  #  
