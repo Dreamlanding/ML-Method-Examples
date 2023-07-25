3d18dbf02a6384c392a64602bd3591955f7e3baf
Dreamlanding <112757530+Dreamlanding@users.noreply.github.com>
2023-07-25 13:58:41 +0000

    Update mikrotik-new.rsc

--- tensor_flow/save_and_restore/save_reuse_parameters.py
+++ tensor_flow/save_and_restore/save_reuse_parameters.py
@@ -3,4 +3,8 @@
 import numpy as np
 from sklearn.datasets import fetch_california_housing
 import tensorflow as tf
-from sklearn.preprocessing import StandardScaler  #  
+from sklearn.preprocessing import StandardScaler  #  用于数据缩放
+
+housing = fetch_california_housing()
+m, n = housing.data.shape  # m是样本数，n是特征的数量
+print(
