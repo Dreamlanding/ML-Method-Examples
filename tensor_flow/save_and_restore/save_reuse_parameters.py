8bee1b2b13a4fa0b5d41e6d3876e9fb6719c944f
Dreamlanding <112757530+Dreamlanding@users.noreply.github.com>
2023-10-02 15:47:46 +0000

    memcached

--- tensor_flow/save_and_restore/save_reuse_parameters.py
+++ tensor_flow/save_and_restore/save_reuse_parameters.py
@@ -48,4 +48,6 @@ def train_theta_by_gd_load_para(X, y):
     # n_epochs = 1000  # 迭代次数
     # learning_rate = 0.01  # 之前学习率不能太大是因为X没有做缩放
     theta = tf.Variable(tf.random_uniform([n + 1, 1], -1.0, 1.0), name='theta')
-    y_pred
+    y_pred = tf.matmul(X, theta, name='predictions')
+    error = y_pred - y
+    mse = tf.reduce_mean(tf.s
