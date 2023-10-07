c9a05e0971b8e921da812d94d9514af864b7aab8
Dreamlanding <112757530+Dreamlanding@users.noreply.github.com>
2023-10-07 08:31:59 +0000

    fix flake 8

--- tensor_flow/save_and_restore/save_reuse_parameters.py
+++ tensor_flow/save_and_restore/save_reuse_parameters.py
@@ -56,4 +56,6 @@ def train_theta_by_gd_load_para(X, y):
     # init = tf.global_variables_initializer()
     saver = tf.train.Saver()  # create a Saver node after all variable nodes are created
     with tf.Session() as sess:
-        saver.restore(sess, 'cv/my_model_final.ckpt')  # 加载上面保存的final c
+        saver.restore(sess, 'cv/my_model_final.ckpt')  # 加载上面保存的final check point
+        # 在session中可以直接使用这些导入的参数进行运算
+        mse, error, theta = s
