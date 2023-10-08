fd8fde453460f97caaf46912d981cca62c71e6cd
Dreamlanding <112757530+Dreamlanding@users.noreply.github.com>
2023-10-08 14:40:57 +0000

    Updating.

--- tensor_flow/use_tensorboard/visualize_graph_and_training_curve.py
+++ tensor_flow/use_tensorboard/visualize_graph_and_training_curve.py
@@ -1,1 +1,3 @@
-# this example comes from book, "Machine Learning 
+# this example comes from book, "Machine Learning with Scikit-Learn and TensorFlow"
+# 程序运行结束后，再命令行中执行tensorboard --logdir tf_logs/
+# 然后再http
