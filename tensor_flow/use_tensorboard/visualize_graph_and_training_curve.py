122976569e61986fcf7ba7c09a3ba7b2bf8ccb4d
Dreamlanding <112757530+Dreamlanding@users.noreply.github.com>
2023-10-11 08:56:42 +0000

    updated responsefiles

--- tensor_flow/use_tensorboard/visualize_graph_and_training_curve.py
+++ tensor_flow/use_tensorboard/visualize_graph_and_training_curve.py
@@ -1,3 +1,6 @@
 # this example comes from book, "Machine Learning with Scikit-Learn and TensorFlow"
 # 程序运行结束后，再命令行中执行tensorboard --logdir tf_logs/
-# 然后再http
+# 然后再http://localhost:6006/可以查看面板
+
+import numpy as np
+from sklearn.datasets import fetch_california_h
