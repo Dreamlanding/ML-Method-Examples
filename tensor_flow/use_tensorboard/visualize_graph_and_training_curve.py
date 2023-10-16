090c9aa00ac15c8e2646c76618739292e5d78b03
Dreamlanding <112757530+Dreamlanding@users.noreply.github.com>
2023-10-16 14:57:40 +0000

    Fix method name.

--- tensor_flow/use_tensorboard/visualize_graph_and_training_curve.py
+++ tensor_flow/use_tensorboard/visualize_graph_and_training_curve.py
@@ -5,4 +5,8 @@
 import numpy as np
 from sklearn.datasets import fetch_california_housing
 import tensorflow as tf
-from sklearn.preprocessing import StandardScaler  #  
+from sklearn.preprocessing import StandardScaler  #  用于数据缩放
+from datetime import datetime
+
+# 按照时间创建存放log文件的文件夹，用于tensorBoard的可视化
+now = datetime.utcnow().
